b77582cf3e1824af8b4b597547597c6fbc9e9c23
federicocecconi <fedececco9.github@manyme.com>
2021-10-27 11:57:06 +0200

    commit

--- src/index-dev.js
+++ src/index-dev.js
@@ -1,6 +1,17 @@
+async function requestPollutionData() {
+    // la nostra chiave è al sicuro nelle Environment Variables
+    const API_KEY = process.env.API_KEY
+  
+    // la parola chiave è await: è quella che dice a JS di fermarsi a questa riga finchè la fetch API non restituisce il risultato della richiesta
+    const response = await fetch(`/* ... */${API_KEY}`) // non scriverò COME fare la richiesta, va capito dalla documentazione https://aqicn.org/json-api/doc/
+    const data = await response.json()
+  
+    console.log(data) // facciamo ciò che vogliamo con i dati ottenuti: scelta, elaborazione, visualizzazione..
+  }
+  
+  requestPollutionData() 
 
-
- const container = document.body.querySelector(".container"),
+const container = document.body.querySelector(".container"),
 inputPart = container.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-txt"),
 inputField = inputPart.querySelector("input"),
